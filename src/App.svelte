32b2d213a8a7baf1209a4d459aff398233d439f1
Simo-Pekka Koskinen <simo.pekka.koskinen@gmail.com>
2022-05-22 22:22:57 +0300

    Added toasts

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,28 @@
 <script lang="ts">
 	import SveltyPicker from 'svelty-picker'
+	import { SvelteToast, toast } from '@zerodevx/svelte-toast';
+	import { success, failure } from './toasts'
 	let selectedTime;
+
+	const toastOptions = {}
 
 	function setSelectedTime(event) {
 		selectedTime = event.detail
 	}
 	async function onSubmit(e: any) {
-		console.log("FORM DATA", selectedTime)
-		try {
-			await fetch('https://9bc8zpr105.execute-api.eu-west-1.amazonaws.com/marjukanaika-mailsender', {
+		const response = await fetch('https://9bc8zpr105.execute-api.eu-west-1.amazonaws.com/marjukanaika-mailsender', {
 			method: 'POST',
-			body: JSON.stringify({time: selectedTime}),
+			body: JSON.stringify({ time: selectedTime }),
 			headers: {
 				'Content-Type': 'application/json'
 			}
 		})
-		} catch(e) {
+		console.log("RESPONSE", response)
+		if (response.ok) {
+			success("Booked some Marjukka-time!")
+		} else {
 			console.log("Cannot send email", e)
+			failure("Yikes, something went wrong while booking time!")
 		}
 		
 	}
@@ -26,12 +32,13 @@
 	<h1>Moicca Marjukka!</h1>
 	<p>Tästä pääset varaamaan tunnin omaa aikaa haluamanasi ajankohtana :)</p>
 	<form on:submit|preventDefault={onSubmit}>
-		<label for="time">
+		<label for="time" class="time-select">
 			<span>Valittu aika:</span>
 			<SveltyPicker format="dd.mm.yyyy hh:mm" name="time" required on:change={setSelectedTime}></SveltyPicker>
 		</label>
 		<button type="submit">Varaa!</button>
 	</form>
+	<SvelteToast options={toastOptions} />
 </main>
 
 <style>
@@ -49,6 +56,25 @@
 	}
 
 	p {
+		margin: 2rem 0rem;
+	}
+
+	button {
+		cursor: pointer;
+		padding: 0.5em 1em;
+		background-color: skyblue;
+		width: unset;
+		border-radius: 6px;
+		font-size: 1.5rem;
+		transition: all 0.2s ease-in-out;
+	}
+
+	button:hover {
+		background-color: blueviolet;
+		color: white;
+	}
+
+	.time-select {
 		margin: 2rem 0rem;
 	}
 
